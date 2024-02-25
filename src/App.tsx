51ca41e704c291011c130ccf49a796a3321c5494
kyuuseiryuu <kyuuseiryuu@gmail.com>
2024-02-24 17:54:14 -0800

    add clear and save button

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import { Button, Space } from 'antd'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const KEY_SAVE_NUMBER = 'KEY_SAVE_NUMBER';
+
+function App() {
+  const [count, setCount] = useState<number>(0)
+  const handleSave = useCallback(() => {
+    localStorage.setItem(KEY_SAVE_NUMBER, count.toString());
+  }, [count]);
+  const renderdRef = useRef(false);
+  useEffect(() => {
+    if (renderdRef.current) return;
+    renderdRef.current = true;
+    console.debug('useEffect...');
+    const savedNumber = Number(localStorage.getItem(KEY_SAVE_NUMBER)) || 0;
+    console.debug('saved number', savedNumber);
+    setCount(savedNumber);
+  }, []);
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Space direction='vertical'>
+          <Button type='primary' onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+          <Button type='primary' danger onClick={() => setCount(0)}>
+            Reset
+          </Button>
+          <Button danger onClick={handleSave}>
+            Save
+          </Button>
+        </Space>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
